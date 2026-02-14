32057fe6b64d7b9b60df8ebb60d2a93dd23c6d6b
Raj Agrahari <raj.agr2002@gmail.com>
2026-02-14 23:52:43 +0530

    CODE

---
+++ Array/TakeINputFromUser.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TakeINputFromUser {
+    public static void main(String[] args) {
+        int marks[] =new int [100];
+        Scanner sc= new Scanner(System.in);
+        marks[0]=sc.nextInt();
+        marks[1]=sc.nextInt();
+        marks[1]=sc.nextInt();
+
+        System.out.println("Phy : " + marks[0]);
+        System.out.println("Math : " + marks[0]);
+
+
+    }
+}
+ 
